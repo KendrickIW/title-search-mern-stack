902cd692e89d5abec2c0e1f759fe47654f3464d0
Kendrick Williams <williams.kendrick@gmail.com>
2018-09-15 20:07:07 -0400

    Added first failing test

--- test/routes/index.test.ts
+++ test/routes/index.test.ts
@@ -1,3 +1,15 @@
+import * as chai from 'chai';
+import chaiExpressRouter from 'chai-express-router';
+import routes from '../../routes';
+
+chai.use(chaiExpressRouter);
+
 describe('routes', () => {
-  it('exists');
+  it('exists', () => {
+    routes.should.not.be.undefined;
+  });
+
+  it('has a get \'/\' route', () => {
+    routes.should.have.route("get", "/");
+  });
 });
